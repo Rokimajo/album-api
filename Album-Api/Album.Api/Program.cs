875b4ce18640d5fb93199bc2dd62b14cd48855c9
Rokimajo <robinvliegtvandaag@gmail.com>
2024-05-16 15:57:17 +0200

    Final Program change.

--- Album-Api/Album.Api/Program.cs
+++ Album-Api/Album.Api/Program.cs
@@ -8,23 +8,7 @@ public class Program {
         builder.Services.AddTransient<FakeGreetingService>();
         // Add services to the container.
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
         app.MapControllers();
         app.Run();
         Console.WriteLine($"{DateTime.Now} | Application shut down.");
